5ad62500f14291e82951ec03a0dd4953a9f25f93
Amit0785 <amit.singh@brainiuminfotech.com>
2022-10-20 17:55:49 +0530

    podfile data added

--- App/Constant.js
+++ App/Constant.js
@@ -2,3 +2,13 @@ export const appId = 'd36ce652-a704-4092-a6a2-c79cda8094d7';
 
 export const device_token =
   'd3ZUYe_1SK2wsi1PidFQio:APA91bG5b5ISzPi276zR6cxdMhcniUwmCtSdFD4ewOW1uBIyHqp3lWtNxPKTd7C-wpJPFyG9CU6jSPX3RkWdQOAkFBA1dDUqv6qPE0jTC_4O-Y-RhFV1N34t9cnGUP7wbzF9MNtjJOP1';
+
+// podfile data
+
+// post_install do |installer|
+// installer.pods_project.targets.each do |target|
+//   target.build_configurations.each do |config|
+//     config.build_settings['EXCLUDED_ARCHS[sdk=iphonesimulator*]'] = "arm64"
+//   end
+// end
+// end
