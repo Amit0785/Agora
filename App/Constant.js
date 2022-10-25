96ed37c708f5b001190dd0101aaf4c97f9acf769
Amit0785 <amit.singh@brainiuminfotech.com>
2022-10-25 13:19:54 +0530

    React native ios push notification doc and code for M1 chip

--- App/Constant.js
+++ App/Constant.js
@@ -3,7 +3,7 @@ export const appId = 'd36ce652-a704-4092-a6a2-c79cda8094d7';
 export const device_token =
   'd3ZUYe_1SK2wsi1PidFQio:APA91bG5b5ISzPi276zR6cxdMhcniUwmCtSdFD4ewOW1uBIyHqp3lWtNxPKTd7C-wpJPFyG9CU6jSPX3RkWdQOAkFBA1dDUqv6qPE0jTC_4O-Y-RhFV1N34t9cnGUP7wbzF9MNtjJOP1';
 
-// podfile data
+// + podfile data for M1 chip
 
 // post_install do |installer|
 // installer.pods_project.targets.each do |target|
