029001bec50fb4f33e04f521a15f2b5468b7f351
Amit0785 <amit.singh@brainiuminfotech.com>
2022-12-23 13:26:58 +0530

    Appp icon2

--- App/AgoraUrl.js
+++ App/AgoraUrl.js
@@ -96,3 +96,7 @@ const googleSignInDoc =
 //udid link='https://udid.tech/'
 
 //new app icon='https://makeappicon.com/'
+
+//new app icon for android='https://www.appicon.co/#image-sets'
+
+//design icon='https://romannurik.github.io/AndroidAssetStudio/icons-launcher.html#foreground.type=image&foreground.space.trim=1&foreground.space.pad=0.25&foreColor=rgba(96%2C%20125%2C%20139%2C%200)&backColor=rgb(255%2C%20255%2C%20255)&crop=0&backgroundShape=circle&effects=none&name=ic_launcher
